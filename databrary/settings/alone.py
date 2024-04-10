f8e3de29e320a4ac4adb2c0f315322eebc33513b
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2024-04-10 19:40:15 +0200

    cleanup

--- databrary/settings/alone.py
+++ databrary/settings/alone.py
@@ -14,15 +14,3 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": (os.getenv("POSTGRES_DB", "databrary")),
-#         "USER": (os.getenv("POSTGRES_USER", "databrary")),
-#         "PASSWORD": (os.getenv("POSTGRES_PASSWORD", "PinkMist")),
-#         "HOST": (os.getenv("POSTGRES_HOST", "databrary-db")),
-#         "PORT": (os.getenv("POSTGRES_PORT", "5432")),
-#     }
-# }
