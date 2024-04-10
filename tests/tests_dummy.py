a0c159188c48d9540ffc6bcc2611c72f7a93b284
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2024-04-10 04:38:52 +0200

    configure test and coverage add django-stubs for static typechekcing

---
+++ tests/tests_dummy.py
@@ -0,0 +1,12 @@
+# TODO: remove this file once first test case is added
+
+from django.test import TestCase
+
+
+class AnimalTestCase(TestCase):
+    def test_animals_can_speak(self):
+        self.assertTrue(True)
+
+
+def test_dummy():
+    assert True
